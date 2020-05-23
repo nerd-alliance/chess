17132865582712547b711a31734939c4f6a9b526
domroutley <dominic.routley@gmail.com>
2020-05-23 14:15:06 +0100

    adds (broken) basic flask app

---
+++ app/main.py
@@ -0,0 +1,12 @@
+import jinja2 as j2
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def demo():
+    return render_template('./html/game.html.j2', title='Hello world', content='content')
+
+
+if __name__ == '__main__':
+    app.run()
