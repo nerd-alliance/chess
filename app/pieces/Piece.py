cd75b566afa463b4230f809594eb63c9fe767bcb
domroutley <dominic.routley@gmail.com>
2020-05-24 19:54:23 +0100

    adds: Piece initial functions

--- app/pieces/Piece.py
+++ app/pieces/Piece.py
@@ -1,2 +1,14 @@
 class Piece:
-    pass
+    valid_factions = ("blanc", "noir")
+    def __init__(self, faction, coord):
+        if faction.lower() not in Piece.valid_factions:
+            raise ValueError(f"{faction} is not a valid faction. Valid factions are {Piece.valid_factions}")
+        else:
+            self.faction = faction.lower()
+        self.coord = coord
+
+    def get_faction(self):
+        return self.faction
+
+    def get_coord(self):
+        return self.coord
