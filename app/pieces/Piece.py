3203bad3f2b6449c0dc4ad5abd130b060f257289
domroutley <dominic.routley@gmail.com>
2020-05-25 02:55:33 +0100

    adds: move, get_valid_moves funcs to Piece

--- app/pieces/Piece.py
+++ app/pieces/Piece.py
@@ -1,6 +1,6 @@
 class Piece:
-    valid_factions = ("blanc", "noir")
     def __init__(self, faction, coord):
+        valid_factions = ("blanc", "noir")
         if faction.lower() not in Piece.valid_factions:
             raise ValueError(f"{faction} is not a valid faction. Valid factions are {Piece.valid_factions}")
         else:
@@ -12,3 +12,19 @@ class Piece:
 
     def get_coord(self):
         return self.coord
+
+    def move(self, coord, dry_run=False):
+        if coord in self.get_valid_moves():
+            if not dry_run:
+                self.coord = coord
+            return True
+        else:
+            return False
+
+    def get_valid_moves(self):
+        """
+        Exists so that move() does not syntax fail.
+        However should be overwritten by extending piece,
+        currently will force move() to return False
+        """
+        return []
