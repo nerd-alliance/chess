2d9732c73cbe50b642046635168f491349a115f5
domroutley <dominic.routley@gmail.com>
2020-06-10 17:33:53 +0100

    chore: readds (and fixes a bit) the get valid moves logic for the pawn

--- app/Pawn.py
+++ app/Pawn.py
@@ -1,8 +1,9 @@
 from Piece import Piece
+from Coord import Coord
 
 class Pawn(Piece):
 
-    def get_valid_moves(self, board) -> list:
+    def get_valid_moves(self, board):
         """
         Returns a list of valid coords that the pawn can move to.
         """
@@ -10,15 +11,27 @@ class Pawn(Piece):
         valid_moves = []
 
         # If white pawn is still in initial row
-        if self.faction == "blanc" and self.coord.row == '2':
-            pass
+        if self.faction == "blanc" and self.coord.row == "2":
+            if not __piece_in_location(Coord(self.coord.column, "3"), board):
+                valid_moves.append(Coord(self.coord.column, "3"))
+            if not __piece_in_location(Coord(self.coord.column, "4"), board):
+                valid_moves.append(Coord(self.coord.column, "4"))
 
-        elif self.faction == "noir" and self.coord.row == '7':
+        elif self.faction == "noir" and self.coord.row == "7":
             # If black pawn is still in initial row
-            pass
+            if not __piece_in_location(Coord(self.coord.column, "6"), board):
+                valid_moves.append(Coord(self.coord.column, "6"))
+            if not __piece_in_location(Coord(self.coord.column, "5"), board):
+                valid_moves.append(Coord(self.coord.column, "5"))
 
         else:
-            pass
+            single_ahead = int(self.coord.row) + self.forward
+            if (
+                str(single_ahead) in ('1','2','3','4','5','6','7','8')
+                and
+                not __piece_in_location(Coord(single_ahead, self.coord.column), board)
+            ):
+                valid_moves.append(Coord(str(single_ahead), self.coord.column))
 
         ##0 TODO: Add valid moves for if a diagonal piece can be taken
 
