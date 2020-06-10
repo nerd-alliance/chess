3b05a8c60c744e80016c6b8d04f83808855067ec
domroutley <dominic.routley@gmail.com>
2020-06-10 15:45:40 +0100

    fix: self reference

--- app/Coord.py
+++ app/Coord.py
@@ -2,9 +2,9 @@ class Coord:
     row = None
     column = None
 
-    def __init__(self, inRow, inColumn):
+    def __init__(self, row, column):
         if inRow in ('1','2','3','4','5','6','7','8'):
-            row = inRow
+            self.row = row
         else:
             raise ValueError(
                 f"{str(inRow)} is not a valid row.\n"
@@ -12,7 +12,7 @@ class Coord:
             )
 
         if inColumn in ('a','b','c','d','e','f','g','h'):
-            column = inColumn
+            self.column = column
         else:
             raise ValueError(
                 f"{str(inColumn)} is not a valid column.\n"
